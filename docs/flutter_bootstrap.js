3248ceb894e286b1683b6b2d3a90678872703683
SafeShareGitHub <SafeShareGithub@gmail.com>
2025-10-03 16:14:51 +0200

    title editable

--- docs/flutter_bootstrap.js
+++ docs/flutter_bootstrap.js
@@ -38,6 +38,6 @@ _flutter.buildConfig = {"engineRevision":"c29809135135e262a912cf583b2c90deb9ded6
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "332093979"
+    serviceWorkerVersion: "4118284054"
   }
 });
